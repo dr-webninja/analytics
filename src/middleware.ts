8d8ecffcb728ef865d47ac0255ff43ea631c16a7
Daniel Reis <dr.webninja@gmail.com>
2024-09-06 10:06:35 +0100

    cenas

---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+  const xpto = await fetch("https://jsonplaceholder.typicode.com/todos/1").then(
+    (response) => response.json()
+  );
+
+  console.log("MIDDLEWARE", request.nextUrl.pathname);
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
+};
